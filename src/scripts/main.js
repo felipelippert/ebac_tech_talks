340d79bb5cb619394e418a778a9b1801dccaa1b7
unknown <felipe.v.lippert@gmail.com>
2023-11-26 17:31:46 -0300

    finished landing page

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,28 @@
-console.log("Hello, World")
+AOS.init();
+
+const eventDate = new Date("Dec 10, 2023, 19:00:00");
+const eventTimeStamp = eventDate.getTime();
+
+const countdown = setInterval(function() {
+    const now = new Date();
+    const currentTimeStamp = now.getTime();
+
+    const timeUntilEvent = eventTimeStamp - currentTimeStamp;
+
+    const dayInMs = 1000 * 60 * 60 * 24;
+    const hoursInMs = 100 * 60 * 60;
+    const minutesInMs = 1000 * 60;
+    
+    const daysRemaining = Math.floor(timeUntilEvent / dayInMs);
+    const hoursRemaining = Math.floor((timeUntilEvent % dayInMs) / hoursInMs);
+    const minutesRemaining = Math.floor((timeUntilEvent % hoursInMs) / minutesInMs);
+    const secondsRemaining = Math.floor((timeUntilEvent % minutesInMs) / 1000);
+
+    document.getElementById('countdown').innerHTML = `${daysRemaining}d ${hoursRemaining}h ${minutesRemaining}m ${secondsRemaining}s`
+
+    if (timeUntilEvent < 0) {
+        clearInterval(countdown);
+        document.getElementById('countdown').innerHTML = 'Evento expirado'
+    }
+
+}, 1000);
